4a0cf003f8a82b21b59a16ba3ee07e089035efde
osha7 <ogroetz@gmail.com>
2022-04-11 00:10:21 +0000

    tests pass

--- class_person.rb
+++ class_person.rb
@@ -5,7 +5,23 @@
 #  - An attribute birthdate (we just assign a string to this attribute, e.g. "April 19, 1987")
 #  - An instance method, full_name, that puts first_name and last_name together
 #  - An instance method, age, that calculates the number of years between today and birthdate
+require "date"
 
 class Person
-  
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :birthdate
+
+  def full_name
+    return self.first_name + " " + self.last_name
+  end
+
+  def age
+    birthdate_parsed = Date.parse(self.birthdate)
+    todays_date = Date.today
+    date_difference = todays_date - birthdate_parsed
+    number_of_days = date_difference.to_i
+    number_of_years_old = number_of_days / 365
+    return number_of_years_old
+  end
 end
